e00e345dbee4bc535de140126a766c9a80f1251f
Arnontu <arnontu@gmail.com>
2018-05-18 13:23:07 +0300

    merge
    merge additional functions from "helper"

--- src/oop/ex4/data_structures/AVLTree.java
+++ src/oop/ex4/data_structures/AVLTree.java
@@ -7,6 +7,9 @@ import java.util.Iterator;
  */
 
 public class AVLTree implements Tree {
+	private static final int VIOLATION_1_LEFT = 2;
+	private static final int VIOLATION_1_RIGHT = -2;
+	private static final int VIOLATION_2_DIFFERENT_SIDE = -1;
 	private static final int AVL_H0_MIN_NODES = 1;
 	private static final int AVL_H1_MIN_NODES = 2;
 	/**
@@ -88,7 +91,13 @@ public class AVLTree implements Tree {
 		 * Updates node's balance factor.
 		 */
 		private void updateBalanceFactor() {
-			return;
+			int checkLeft = -1;
+			int checkeRight = -1;
+			if (leftSon != null)
+				checkLeft = leftSon.balanceFactor;
+			if (rightSon != null)
+				checkeRight = rightSon.balanceFactor;
+			balanceFactor = checkLeft + checkeRight;
 		}
 	}
 
@@ -172,7 +181,7 @@ public class AVLTree implements Tree {
 			}
 		}
 		if (operation != Operator.CONTAINS && retValue == SUCCESS_FLAG) {
-			checkDisorder();
+			checkDisorder(currentNode);
 		}
 		return retValue;
 	}
@@ -368,8 +377,57 @@ public class AVLTree implements Tree {
 	}
 
 
-	private void checkDisorder() {
 
+	private void checkDisorder(AvlNode node) {
+		int currentBalance = node.balanceFactor;
+		if (currentBalance == VIOLATION_1_LEFT) {
+			if (node.leftSon.balanceFactor == VIOLATION_2_DIFFERENT_SIDE) {
+				rotateLeft(node.leftSon);
+			}
+			rotateRight(node);
+		}
+		if (currentBalance == VIOLATION_1_RIGHT) {
+			if (node.rightSon.balanceFactor == VIOLATION_2_DIFFERENT_SIDE) {
+				rotateRight(node.rightSon);
+			}
+			rotateLeft(node);
+		}
+	}
+	private void rotateRight(AvlNode node) {
+		AvlNode currentLeft = node.leftSon;
+		node.leftSon = currentLeft.rightSon;
+		currentLeft.parent = node.parent;
+		if (node.parent == null)
+			root = currentLeft;
+		else {
+			if (node == node.parent.rightSon)
+				node.parent.rightSon = currentLeft;
+			else
+				node.parent.leftSon = currentLeft;
+		}
+		currentLeft.rightSon = node;
+		node.parent = currentLeft;
+		node.updateBalanceFactor();
+		currentLeft.updateBalanceFactor();
+
+	}
+
+	private void rotateLeft(AvlNode node) {
+		AvlNode currentRight = node.rightSon;
+		node.leftSon = currentRight.leftSon;
+		currentRight.parent = node.parent;
+		if (node.parent == null) {
+			root = currentRight;
+		} else {
+			if (node == node.parent.rightSon)
+				node.parent.rightSon = currentRight;
+			else
+				node.parent.leftSon = currentRight;
+		}
+		currentRight.leftSon = node;
+		node.parent = currentRight;
+		currentRight.updateBalanceFactor();
+		node.updateBalanceFactor();
 	}
 
 
