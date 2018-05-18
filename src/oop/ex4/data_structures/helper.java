803a88ecf098de89168c26a847c0a465b49f2cf4
Arnontu <arnontu@gmail.com>
2018-05-18 15:06:13 +0300

    update
    deep copy func
    iterator func

--- src/oop/ex4/data_structures/helper.java
+++ src/oop/ex4/data_structures/helper.java
@@ -1,68 +1,39 @@
 package oop.ex4.data_structures;
 
 public class helper {
-	private static final int VIOLATION_1_LEFT = 2;
-	private static final int VIOLATION_1_RIGHT = -2;
-	private static final int VIOLATION_2_DIFFERENT_SIDE = -1;
-	private void checkDisorder(AvlNode node) {
-		int currentBalance = node.balanceFactor;
-		if (currentBalance == VIOLATION_1_LEFT) {
-			if (node.leftSon.balanceFactor == VIOLATION_2_DIFFERENT_SIDE) {
-				rotateLeft(node.leftSon);
-			}
-			rotateRight(node);
+	public AVLTree(AVLTree tree) {
+		AVLTree copyTree = new AVLTree();
+		Iterator myIter=tree.iterator();
+		while (myIter.hasNext()){
+			copyTree.add((int)myIter.next());
 		}
-		if (currentBalance == VIOLATION_1_RIGHT) {
-			if (node.rightSon.balanceFactor == VIOLATION_2_DIFFERENT_SIDE) {
-				rotateRight(node.rightSon);
-			}
-			rotateLeft(node);
-		}
+		this.root=copyTree.root;
 	}
+	private class AVLTreeIterator implements Iterator<Integer> {
 
-	private void rotateRight(AvlNode node) {
-		AvlNode currentLeft = node.leftSon;
-		node.leftSon = currentLeft.rightSon;
-		currentLeft.parent = node.parent;
-		if (node.parent == null)
-			root = currentLeft;
-		else {
-			if (node == node.parent.rightSon)
-				node.parent.rightSon = currentLeft;
-			else
-				node.parent.leftSon = currentLeft;
+		private AvlNode currentNode;
+		private AvlNode lastNode;
+
+		private AVLTreeIterator() {
+			currentNode = root;
 		}
-		currentLeft.rightSon = node;
-		node.parent = currentLeft;
-		node.updateBalanceFactor();
-		currentLeft.updateBalanceFactor();
-
-	}
-
-	private void rotateLeft(AvlNode node) {
-		AvlNode currentRight = node.rightSon;
-		node.leftSon = currentRight.leftSon;
-		currentRight.parent = node.parent;
-		if (node.parent == null) {
-			root = currentRight;
-		} else {
-			if (node == node.parent.rightSon)
-				node.parent.rightSon = currentRight;
-			else
-				node.parent.leftSon = currentRight;
+		/**
+		 * @return Returns true if current node has a son.
+		 */
+		@Override
+		public boolean hasNext() {
+			AvlNode nextNode=successor(currentNode);
+			if(nextNode!=null){
+				lastNode=nextNode;
+				return true;}
+			return false;
 		}
-		currentRight.leftSon = node;
-		node.parent = currentRight;
-		currentRight.updateBalanceFactor();
-		node.updateBalanceFactor();
-	}
-	private void updateBalanceFactor() {
-		int checkLeft = -1;
-		int checkeRight = -1;
-		if (leftSon != null)
-			checkLeft = leftSon.balanceFactor;
-		if (rightSon != null)
-			checkeRight = rightSon.balanceFactor;
-		balanceFactor = checkLeft + checkeRight;
+		@Override
+		public Integer next() {
+			if (successor(currentNode)==null)
+				throw new NoSuchElementException();
+			currentNode=lastNode;
+			return lastNode.value;
+		}
 	}
 }
